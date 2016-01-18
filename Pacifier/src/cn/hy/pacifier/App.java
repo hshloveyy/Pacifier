11a6d52e23a590cef7d599637f5f30ed2be2935c
hshloveyy <386184368@qq.com>
2016-01-18 10:24:27 +0800

    add

---
+++ Pacifier/src/cn/hy/pacifier/App.java
@@ -0,0 +1,19 @@
+/*
+ * 文件名：App.java
+ * 版权：Copyright 2011-2018 Kerrunt Tech. Co. Ltd. All Rights Reserved. 
+ * 描述：KURRENT系统系列
+ */
+package cn.hy.pacifier;
+/**
+ * 修改人： Heshaohua
+ * 修改时间：2016年1月18日 上午10:23:54 
+ * 修改内容：新增 
+ * 类说明：
+ */
+
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World~!");
+	}
+}
