c8322c61cf07d947bd01fa1f6320dbaa5d394b6f
Administrator <Administrator@WIN-Admin>
2016-05-31 16:20:24 +0800

    adfa

--- Pacifier/src/com/sea/controller/TestController.java
+++ Pacifier/src/com/sea/controller/TestController.java
@@ -1,4 +1,4 @@
-package cn.hy.pacifier.controller;
+package com.sea.controller;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,17 +8,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("test")
 public class TestController {
 
 	@ResponseBody
-	@RequestMapping("map")
+	@RequestMapping("new")
 	public Map<String, Object> test(){
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("a", "a");
-		map.put("b", "b");
-		map.put("c", "c");
-		map.put("d", "d");
+		map.put("a", "字母");
+		map.put("b", "数字");
+		map.put("c", "我去");
+		map.put("d", "打野");
 		return map;
 	}
 }
